38e8384db4e3d07a7376ac7c4916945cf343e03d
goater <goater1095@gmail.com>
2020-11-27 02:21:26 +0800

    Install passport and passport-local & Add feature: signInPage, signIn and logout

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('./config/passport');
 const app = express();
 const port = 3000;
 
@@ -10,6 +11,8 @@ app.set('view engine', 'handlebars');
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use((req, res, next) => {
   //產生local變數，並可以用req.flash 寫入
@@ -22,6 +25,6 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-require('./routes')(app); //注意此寫法
+require('./routes')(app, passport); //注意此寫法
 
 module.exports = app;

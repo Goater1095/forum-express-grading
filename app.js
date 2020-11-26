b01963b8e6b2fbfc5a32fe145f40ecda186b293a
goater <goater1095@gmail.com>
2020-11-27 01:17:03 +0800

    Install express-session connect-flash & Confirm user password and unique email

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const flash = require('connect-flash');
+const session = require('express-session');
 const app = express();
 const port = 3000;
 
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
+app.use(flash());
+
+app.use((req, res, next) => {
+  //產生local變數，並可以用req.flash 寫入
+  res.locals.success_messages = req.flash('success_messages');
+  res.locals.error_messages = req.flash('error_messages');
+  next();
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);

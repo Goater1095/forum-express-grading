2c6a3c4b720ab35ec33243f71fa1f5776d5c1dbd
goater <goater1095@gmail.com>
2020-11-27 00:33:26 +0800

    Install bcrypt &　Add userController and signup page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000;
 
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);

46c66330e6685ea60fca9a1b1a8a845f00762d65
goater <goater1095@gmail.com>
2021-01-06 18:16:53 +0800

    Install pg and update production config & Listen to process.env.PORT & define Procfile

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const methodOverride = require('method-override');
 const passport = require('./config/passport');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');

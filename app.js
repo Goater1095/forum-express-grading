846adc62e5b790c9b1c453d944152c0b5b0b5352
goater <goater1095@gmail.com>
2021-01-07 10:37:41 +0800

    Install Imgur api and update admin POST and PUT & Add dotenv

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const passport = require('./config/passport');
 const app = express();
 const port = process.env.PORT || 3000;
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.use(express.urlencoded({ extended: true }));

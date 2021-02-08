85659d4bebc4a327b75a33fb147a4984380f4dd4
goater <goater1095@gmail.com>
2021-02-08 10:52:11 +0800

    Add restaurants show page

--- routes/index.js
+++ routes/index.js
@@ -30,81 +30,26 @@ module.exports = (app, passport) => {
   app.get('/', authenticated, (req, res) => res.redirect('/restaurants'));
   //在 /restaurants 底下則交給 restController.getRestaurants 來處理
   app.get('/restaurants', authenticated, restController.getRestaurants);
+  app.get('/restaurants/:id', authenticated, restController.getRestaurant);
 
   // 連到 /admin 頁面就轉到 /admin/restaurants
-  app.get('/admin', authenticatedAdmin, (req, res) =>
-    res.redirect('/admin/restaurants')
-  );
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'));
   // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
-  app.get(
-    '/admin/restaurants',
-    authenticatedAdmin,
-    adminController.getRestaurants
-  );
-  app.get(
-    '/admin/restaurants/create',
-    authenticatedAdmin,
-    adminController.createRestaurant
-  );
-  app.post(
-    '/admin/restaurants',
-    authenticatedAdmin,
-    upload.single('image'),
-    adminController.postRestaurant
-  );
-  app.get(
-    '/admin/restaurants/:id',
-    authenticatedAdmin,
-    adminController.getRestaurant
-  );
-  app.get(
-    '/admin/restaurants/:id/edit',
-    authenticatedAdmin,
-    adminController.editRestaurant
-  );
-  app.put(
-    '/admin/restaurants/:id',
-    authenticatedAdmin,
-    upload.single('image'),
-    adminController.putRestaurant
-  );
-  app.delete(
-    '/admin/restaurants/:id',
-    authenticatedAdmin,
-    adminController.deleteRestaurant
-  );
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants);
+  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant);
+  app.post('/admin/restaurants', authenticatedAdmin, upload.single('image'), adminController.postRestaurant);
+  app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant);
+  app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant);
+  app.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant);
+  app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant);
   app.get('/admin/users', authenticatedAdmin, adminController.getUsers);
-  app.put(
-    '/admin/users/:id/toggleAdmin',
-    authenticatedAdmin,
-    adminController.toggleAdmin
-  );
+  app.put('/admin/users/:id/toggleAdmin', authenticatedAdmin, adminController.toggleAdmin);
   //categoryController
-  app.get(
-    '/admin/categories',
-    authenticatedAdmin,
-    categoryController.getCategories
-  );
-  app.get(
-    '/admin/categories/:id',
-    authenticatedAdmin,
-    categoryController.getCategories
-  );
-  app.post(
-    '/admin/categories',
-    authenticatedAdmin,
-    categoryController.postCategory
-  );
-  app.put(
-    '/admin/categories/:id',
-    authenticatedAdmin,
-    categoryController.putCategory
-  );
-  app.delete(
-    '/admin/categories/:id',
-    authenticatedAdmin,
-    categoryController.deleteCategory
-  );
+  app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories);
+  app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories);
+  app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory);
+  app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory);
+  app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory);
   //userController
   app.get('/signup', userController.signUpPage);
   app.post('/signup', userController.signUp);

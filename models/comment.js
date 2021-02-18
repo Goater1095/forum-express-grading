e7eec54c1c0492391b207837c0d4404cf3ddfe1b
goater <goater1095@gmail.com>
2021-02-19 02:13:09 +0800

    Add post comment feature on restaurant page

--- models/comment.js
+++ models/comment.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Comment.belongsTo(models.Restaurant);
+      Comment.belongsTo(models.User);
     }
   }
   Comment.init(
